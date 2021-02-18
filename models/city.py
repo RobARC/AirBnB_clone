d7fd1bef794fb5dd8a66024c9a0eb8436ad3b832
RobARC <rrondonc@gmail.com>
2021-02-18 01:54:57 +0000

    update file city.py

--- models/city.py
+++ models/city.py
@@ -11,7 +11,3 @@ class City(BaseModel):
 
     state_id = ""
     name = ""
-
-    def __init__(self, *args, **kwargs):
-        """instatiates a new city"""
-        super().__init__(self, *args, **kwargs)
