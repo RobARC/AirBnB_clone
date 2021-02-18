3846a8b94ed056fe6fb52c307d4498568c068934
RobARC <rrondonc@gmail.com>
2021-02-18 03:29:02 +0000

    uploading test place

---
+++ tests/test_models/test_place.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Unit Test for Place Class
+"""
+import unittest
+from datetime import datetime
+from models.place import Place
+import json
+
+
+class TestPlaceDocs(unittest.TestCase):
+    """Class for testing BaseModel docs"""
+
+    @classmethod
+    def setUpClass(cls):
+        print('\n\n.................................')
+        print('..... Testing Documentation .....')
+        print('........   Place Class   ........')
+        print('.................................\n\n')
+
+    def test_doc_file(self):
+        """... documentation for the file"""
+        expected = 'Place class handles all application places'
+        actual = Place.__doc__
+        self.assertEqual(expected, actual)
+
+    def test_doc_class(self):
+        """... documentation for the class"""
+        expected = 'Place class handles all application places'
+        actual = Place.__doc__
+        self.assertEqual(expected, actual)
